af42d5965dda33c48d2613cae06cf97dc06050a6
luckyligovi <lucky.luvihi@student.moringaschool.com>
2024-05-11 00:05:05 +0300

    operator buttons display once when clicked multiple times

--- calc.js
+++ calc.js
@@ -1,12 +1,30 @@
 const display = document.getElementById("display");
 
-function appendToDisplay(input) {
-  display.value += input;
-  console.log(input);
-}
+// function appendToDisplay(input) {
+//   display.value += input;
+//   console.log(input);
+// }
 
-function clearDisplay() {
-  display.value = "";
+// function clearDisplay() {
+//   display.value = "";
+// }
+
+function appendToDisplay(input){
+    // Check if the input is an operator
+    if (input === '+' || input === '-' || input === '*' || input === '/') {
+        // Check if the last character in the display is already an operator
+        if (display.value.length > 0 && display.value.slice(-1) === '+' || display.value.slice(-1) === '-' || display.value.slice(-1) === '*' || display.value.slice(-1) === '/') {
+            // Replace the existing operator with the new one
+            display.value = display.value.slice(0, -1) + input;
+        } else {
+            // Append the operator to the display
+            display.value += input;
+        }
+    } else {
+        // Append the input to the display
+        display.value += input;
+    }
+    console.log(input);
 }
 
 function calculate() {
