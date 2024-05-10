5526d9a30aaae2383608997afbed3085ee97e276
luckyligovi <lucky.luvihi@student.moringaschool.com>
2024-05-10 23:43:45 +0300

    add delete

--- calc.js
+++ calc.js
@@ -1,21 +1,28 @@
+const display = document.getElementById("display");
 
-const display = document.getElementById("display")
-
-function appendToDisplay(input){
-    display.value += input;
-    console.log(input)
+function appendToDisplay(input) {
+  display.value += input;
+  console.log(input);
 }
 
-
-function clearDisplay(){
-    display.value = ""
+function clearDisplay() {
+  display.value = "";
 }
 
-function calculate(){
-    try{
-        display.value = eval(display.value)
+function calculate() {
+  try {
+    // display.value = eval(display.value)
+    let result = eval(display.value);
+    if (result !== undefined) {
+      display.value = result;
+    } else {
+      display.value = "";
     }
-    catch(error){
-        display.value = "Error"
-    }
+  } catch (error) {
+    display.value = "Error";
+  }
+}
+
+function eraseSingleInput() {
+  display.value = display.value.slice(0, -1);
 }
